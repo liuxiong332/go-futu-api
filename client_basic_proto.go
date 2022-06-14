f653915bbc318b4f925f00c03abcc347262eab2f
liuxiong <liuxiong332@163.com>
2022-06-14 16:54:45 +0800

    Use aes cbc cipher

--- client_basic_proto.go
+++ client_basic_proto.go
@@ -3,22 +3,29 @@ package go_futu_api
 import (
 	"time"
 
+	"github.com/liuxiong332/go-futu-api/pb/common"
 	"github.com/liuxiong332/go-futu-api/pb/initconnect"
 	"github.com/liuxiong332/go-futu-api/pb/keepalive"
 )
 
 func (me *Client) InitConnect() (*initconnect.Response, error) {
+	packetEncAlgo := int32(common.PacketEncAlgo_PacketEncAlgo_None)
+	if me.rsaPrivateKey != nil {
+		packetEncAlgo = int32(common.PacketEncAlgo_PacketEncAlgo_AES_CBC)
+	}
+
 	req := &initconnect.Request{
 		C2S: &initconnect.C2S{
-			ClientID:  &ClientID,
-			ClientVer: &ClientVer,
+			ClientID:      &ClientID,
+			ClientVer:     &ClientVer,
+			PacketEncAlgo: &packetEncAlgo,
 		},
 	}
 	resp := &initconnect.Response{}
 	if err := me.DoRequest(uint32(1001), req, resp); err != nil {
 		return nil, err
 	}
-	if me.rsaPrivateKey != nil && resp.S2C.ConnAESKey != nil {
+	if *resp.RetType == int32(common.RetType_RetType_Succeed) && me.rsaPrivateKey != nil && resp.S2C.ConnAESKey != nil {
 		aesCipher, err := NewAESCipher([]byte(*resp.S2C.ConnAESKey), []byte(*resp.S2C.AesCBCiv))
 		if err != nil {
 			return resp, err
